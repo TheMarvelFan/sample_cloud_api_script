85e6dd54311506a8d8a31d3e8ea7ba844bb07210
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2025-01-22 19:20:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/webhook",(req,res)=>{
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if(mode && token) {
+        if(mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+            console.log("Webhook verified");
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.post("/webhook",(req,res)=>{
+    console.log(req.body);
+    res.status(200).send("Message received");
+});
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Webhook is running");
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+});

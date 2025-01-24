f8a334312d2e2b227c665c89cf3d5a7f3397430f
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2025-01-24 18:11:49 +0530

    trying to send an interactive list type message

--- index.js
+++ index.js
@@ -47,16 +47,130 @@ app.post("/webhook",(req, res)=> {
 
         const phone_number_id = req.body.entry[0].changes[0].value.metadata?.phone_number_id;
 
+        // axios.post(
+        //     `https://graph.facebook.com/v21.0/${phone_number_id}/messages`,
+        //     {
+        //         messaging_product: "whatsapp",
+        //         to: "+" + sender_number_val,
+        //         type: "text",
+        //         text: {
+        //             body: `Hello, ${sender_name}. Your message was: ${message_text}\nIt was received successfully.`
+        //         }
+        //     },
+        //     {
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             Authorization: `Bearer ${process.env.PERMANENT_ACCESS_TOKEN}`
+        //         }
+        //     }
+        // );
+
+        // const message_object = {
+        //     messaging_product: "whatsapp",
+        //     to: `+${sender_number_val}`,
+        //     type: "text",
+        //     text: {
+        //         body: `Hello, ${sender_name}. Your message was: ${message_text}\nIt was received successfully.`
+        //     }
+        // };
+
+        const message_object = {
+            messaging_product: "whatsapp",
+            to: `+${sender_number_val}`,
+            type: "interactive",
+            interactive: {
+                type: "list",
+                header: {
+                  type: "text",
+                  text: "Choose a reply"
+                },
+                body: {
+                    text: `Hello, ${sender_name}. Your message was: ${message_text}\nIt was received successfully. 
+                    Please choose an appropriate reply from the list:`
+                },
+                action: {
+                    button: "Replies",
+                    sections: [],
+                }
+            }
+        };
+
+        const positive_replies = [
+            [
+                "howdy_reply",
+                "Hi, howdy?",
+                "Use this reply if you want to greet the user."
+            ],
+            [
+                "busy_reply",
+                "I am busy right now. Can I call you later?",
+                "Use this reply if you are busy and want to call the user later."
+            ],
+            [
+                "good_reply",
+                "I am good. How are you?",
+                "Use this reply if you are good and want to know how the user is."
+            ]
+        ];
+
+        const negative_replies = [
+            [
+                "who_reply",
+                "How did you get in here?!",
+                "Use this reply if the user is in your room standing behind you"
+            ],
+            [
+                "scream_reply",
+                "I'll scream!",
+                "Use this reply if you are scared by the user's actions and want to scream"
+            ],
+            [
+                "cops_reply",
+                "I am calling the cops!",
+                "Use this reply if you want to scare the user by calling the cops"
+            ]
+        ];
+
+        const sections = [
+            {
+                title: "Positive Replies",
+                rows: []
+            },
+            {
+                title: "Negative Replies",
+                rows: []
+            }
+        ];
+
+        positive_replies.forEach((reply) => {
+            let id = reply[0];
+            let title = reply[1];
+            let description = reply[2];
+
+            sections[0].rows.push({
+                id,
+                title,
+                description
+            });
+        });
+
+        negative_replies.forEach((reply) => {
+            let id = reply[0];
+            let title = reply[1];
+            let description = reply[2];
+
+            sections[1].rows.push({
+                id,
+                title,
+                description
+            });
+        });
+
+        message_object.interactive.action.sections = sections;
+
         axios.post(
             `https://graph.facebook.com/v21.0/${phone_number_id}/messages`,
-            {
-                messaging_product: "whatsapp",
-                to: "+" + sender_number_val,
-                type: "text",
-                text: {
-                    body: `Hello, ${sender_name}. Your message was: ${message_text}\nIt was received successfully.`
-                }
-            },
+            message_object,
             {
                 headers: {
                     "Content-Type": "application/json",

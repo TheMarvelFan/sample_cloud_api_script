c014ac2c9558df34af3d604c8f9c2cf2880114f7
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2025-01-27 17:34:20 +0530

    foolproof response sending

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ app.post("/webhook",(req, res)=> {
             const readableDate = new Date(unixTimestamp * 1000);
             const humanReadableDate = readableDate.toLocaleString();
 
-            const address = JSON.stringify(req.body.entry[0].changes[0].value.messages[0].location, null, "\t");
+            let address = req.body.entry[0].changes[0].value.messages[0].location;
+            address = address || "Address not available!";
 
             const northOrSouth = latitude.toString().charAt(0) === "-" ? "South" : "North";
             const eastOrWest = longitude.toString().charAt(0) === "-" ? "West" : "East";
